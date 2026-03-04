661949ac8e9880eec91dcdb7b41c66cf7d6f2145
Mohd Sakib <mohdsakib@Mohds-MacBook-Air.local>
2026-03-04 11:14:37 +0530

    uncomment db config

--- app/main.py
+++ app/main.py
@@ -4,6 +4,9 @@ from app.core.logging_config import configure_logging, get_logger
 from app.routes.auth_route import router as auth_router
 from app.routes.subscription_route import router as subscription_router
 from app.routes.health_route import router as health_router
+from app.db.database import engine, Base, SessionLocal
+from app.config.settings import settings
+from app.utils.seed import seed_subscription_plans
 
 configure_logging()
 logger = get_logger(__name__)
@@ -44,20 +47,20 @@ async def log_requests(request: Request, call_next):
 @app.on_event("startup")
 def startup_event():
     logger.info("Application startup complete")
-    # required = [
-    #     settings.POSTGRES_USER,
-    #     settings.POSTGRES_PASSWORD,
-    #     settings.POSTGRES_DB,
-    #     settings.DATABASE_HOST,
-    #     settings.DATABASE_PORT,
-    # ]
-    # if any(v in (None, "") for v in required):
-    #     logging.warning("Database env not fully configured; skipping DB init on startup.")
-    #     return
+    required = [
+        settings.POSTGRES_USER,
+        settings.POSTGRES_PASSWORD,
+        settings.POSTGRES_DB,
+        settings.DATABASE_HOST,
+        settings.DATABASE_PORT,
+    ]
+    if any(v in (None, "") for v in required):
+        logger.warning("Database env not fully configured; skipping DB init on startup.")
+        return
 
-    # Base.metadata.create_all(bind=engine)
-    # db = SessionLocal()
-    # try:
-    #     seed_subscription_plans(db)
-    # finally:
-    #     db.close()
+    Base.metadata.create_all(bind=engine)
+    db = SessionLocal()
+    try:
+        seed_subscription_plans(db)
+    finally:
+        db.close()
